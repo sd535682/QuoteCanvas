141bf1fcca4d2e09b822f57c248b352137d3c874
Subhadeep Das <61840607+sd535682@users.noreply.github.com>
2025-05-05 22:06:33 +0530

    Feature/navigation_setup (#1)
    * package installation
    
    * mainactivity.kt file edited for navigation
    
    * setup configuration for navigation flow

--- App.tsx
+++ App.tsx
@@ -1,27 +1,16 @@
 import {useEffect, useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import RootNavigation from './src/navigations/RootNavigation';
 
 export default function App() {
-  const [quotes, setQuotes] = useState<{message: string} | null>(null);
+  const [stageInfo, setStageInfo] = useState<{message: string} | null>(null);
 
   useEffect(() => {
     fetch(`${process.env.API_URL}`)
       .then(response => response.json())
-      .then(data => setQuotes(data));
+      .then(data => setStageInfo(data));
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>{quotes?.message || 'Loading...'}</Text>
-    </View>
-  );
-}
+  console.log(JSON.stringify(stageInfo?.message));
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-});
+  return <RootNavigation />;
+}
---
+++ src/navigations/RootNavigation.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+export default function RootNavigation() {
+  return (
+    <View>
+      <Text>RootNavigation</Text>
+    </View>
+  );
+}
