fc6c18c71c5a45ecaed8d1c823cf9703066bfb07
Subhadeep Das <61840607+sd535682@users.noreply.github.com>
2025-05-06 20:33:27 +0530

    feature/getstarted-screen (#2)

--- src/navigations/RootNavigation.tsx
+++ src/navigations/RootNavigation.tsx
@@ -1,9 +1,21 @@
-import {Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {StatusBar, StyleSheet} from 'react-native';
+import AuthNavigation from './AuthNavigation';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default function RootNavigation() {
   return (
-    <View>
-      <Text>RootNavigation</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <AuthNavigation />
+      </NavigationContainer>
+      <StatusBar backgroundColor="transparent" barStyle={'light-content'} />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- src/screens/AuthScreens/GetStarted.tsx
+++ src/screens/AuthScreens/GetStarted.tsx
@@ -1,9 +1,61 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
 
 export default function GetStarted() {
   return (
-    <View>
-      <Text>GetStarted</Text>
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/getstarted.png')}
+        style={styles.image}
+      />
+      <Text style={styles.title}>Craft Quotes, Keep Memories</Text>
+      <Text style={styles.text}>
+        From books to your personal gallery. Scan pages, design with elegance,
+        and store your favorite literary moments forever.
+      </Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#121315',
+    padding: 20,
+    paddingBottom: 50,
+  },
+  image: {
+    width: '100%',
+    height: '50%',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#6e947b',
+    padding: 16,
+    borderRadius: 50,
+    width: '100%',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#fff',
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 20,
+    color: '#9ca3af',
+    textAlign: 'center',
+    fontWeight: '500',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
