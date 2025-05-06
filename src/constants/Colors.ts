1b26a5eaa1b2f8d86f8c1b452ebd3766ec9300bb
sd535682 <sd535682@gmail.com>
2025-05-06 21:22:49 +0530

    reusable constant colors

---
+++ src/constants/Colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  background: '#121315',
+  white: '#fff',
+  black: '#000',
+  primary: '#6e947b',
+  secondary: '#f5f5f5',
+  text: '#fff',
+  textGray: '#9ca3af',
+  button: '#6e947b',
+};
