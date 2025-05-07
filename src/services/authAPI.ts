1338f88106e487761f908a3d3eb0ed8c2078fd09
sd535682 <sd535682@gmail.com>
2025-05-07 19:17:09 +0530

    api setup

---
+++ src/services/authAPI.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const BASE_URL = process.env.API_URL;
+
+export const authAPI = axios.create({
+  url: BASE_URL,
+  headers: {'Content-Type': 'application/json'},
+});
+
+export interface AuthResponse {
+  accessToken: string;
+  user: {id: 'string'; email: 'string'; password: 'string'};
+}
+
+export async function Login(email: string, password: string) {
+  const response = await authAPI.post<AuthResponse>('/api/v1/auth/login', {
+    email,
+    password,
+  });
+  return response.data;
+}
+
+export async function Register(name: string, email: string, password: string) {
+  const response = await authAPI.post<AuthResponse>('/api/v1/auth/register', {
+    name,
+    email,
+    password,
+  });
+  return response.data;
+}
