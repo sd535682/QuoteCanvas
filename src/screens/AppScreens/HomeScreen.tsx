8af42f96f4ae375bd74b74f179074129f540c344
sd535682 <sd535682@gmail.com>
2025-05-07 22:21:34 +0530

    initial homescreen ui

--- src/screens/AppScreens/HomeScreen.tsx
+++ src/screens/AppScreens/HomeScreen.tsx
@@ -1,9 +1,46 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {Colors} from '../../constants/Colors';
+import {useContext} from 'react';
+import {AuthContext} from '../../context/AuthContext';
 
 export default function HomeScreen() {
+  const {logout} = useContext(AuthContext);
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>HomeScreen</Text>
+      <TouchableOpacity style={styles.button} onPress={() => logout()}>
+        <Text style={styles.buttonText}>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.text,
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: Colors.button,
+    padding: 16,
+    borderRadius: 50,
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: Colors.white,
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
