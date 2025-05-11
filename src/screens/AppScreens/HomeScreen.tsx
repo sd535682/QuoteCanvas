fa659c52fd56bb767220ad529b1cfd4e0b215a99
sd535682 <sd535682@gmail.com>
2025-05-11 22:37:25 +0530

    rendering quotes in home

--- src/screens/AppScreens/HomeScreen.tsx
+++ src/screens/AppScreens/HomeScreen.tsx
@@ -1,10 +1,30 @@
 import {View, Text, StyleSheet} from 'react-native';
 import {Colors} from '../../constants/Colors';
+import {useEffect} from 'react';
+import {getFeed, Quote} from '../../services/feedAPI';
+import {useState} from 'react';
 
 export default function HomeScreen() {
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+  useEffect(() => {
+    getFeed()
+      .then(data => {
+        setQuotes(data?.data || []);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>HomeScreen</Text>
+      {quotes.map((quote, index) => (
+        <View key={index} style={styles.quoteContainer}>
+          <Text style={styles.category}>{quote.category}</Text>
+          <Text style={styles.title}>{quote.quote}</Text>
+          <Text style={styles.author}>~{quote.author}</Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -17,11 +37,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: Colors.text,
+  quoteContainer: {
+    width: '100%',
+    alignItems: 'center',
+    gap: 5,
+    flexDirection: 'column',
+    marginVertical: 10,
+  },
+  category: {
+    fontSize: 12,
+    color: 'white',
     textAlign: 'center',
-    marginTop: 20,
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  },
+  author: {
+    fontSize: 12,
+    color: 'white',
+    textAlign: 'center',
   },
 });
