b62cecacab5aed80d844596f9e83491109181881
sd535682 <sd535682@gmail.com>
2025-05-11 19:09:18 +0530

    basic implementation of floating bottom tab bar

--- src/navigations/AppNavigation.tsx
+++ src/navigations/AppNavigation.tsx
@@ -1,22 +1,57 @@
+import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/AppScreens/HomeScreen';
 import CreateScreen from '../screens/AppScreens/CreateScreen';
 import ProfileScreen from '../screens/AppScreens/ProfileScreen';
+import FloatingTabbar from '../components/navigationcomponents/FloatingTabbar';
+import Lucide from '@react-native-vector-icons/lucide';
 
 export type AppTabParamsList = {
   Home: undefined;
-  Profile: undefined;
   Create: undefined;
+  Profile: undefined;
 };
 
-const Tab = createBottomTabNavigator<AppTabParamsList>();
+const Tab = createBottomTabNavigator();
 
 export default function AppNavigation() {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Create" component={CreateScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+      }}
+      tabBar={props => <FloatingTabbar {...props} />}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({focused}) => (
+            <Lucide name="rss" size={24} color={focused ? '#000' : '#777'} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Create"
+        component={CreateScreen}
+        options={{
+          tabBarLabel: 'Create',
+          tabBarIcon: ({focused}) => (
+            <Lucide name="quote" size={24} color={focused ? '#000' : '#777'} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({focused}) => (
+            <Lucide name="user" size={24} color={focused ? '#000' : '#777'} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
