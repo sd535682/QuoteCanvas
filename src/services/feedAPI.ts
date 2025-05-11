66a87358416e9471416d2302f471cb4a1c5a0ae7
sd535682 <sd535682@gmail.com>
2025-05-11 22:35:32 +0530

    api setup

---
+++ src/services/feedAPI.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import {BASE_URL} from './authAPI';
+import {getToken} from '../utils/authStorage';
+
+export const feedAPI = axios.create({
+  baseURL: BASE_URL,
+  headers: {'Content-Type': 'application/json'},
+});
+
+export interface Quote {
+  _id: string;
+  quote: string;
+  author: string;
+  category: string;
+  user: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface FeedResponse {
+  success: boolean;
+  data: Quote[];
+}
+
+export async function getFeed() {
+  try {
+    const token = await getToken();
+    if (!token) {
+      console.warn('No token found!');
+      return;
+    }
+
+    const response = await feedAPI.get<FeedResponse>('/api/v1/quotes', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log('quotes feedAPI.ts', response.data);
+    return response.data;
+  } catch (error) {
+    console.log('Feed API error:', error);
+  }
+}
