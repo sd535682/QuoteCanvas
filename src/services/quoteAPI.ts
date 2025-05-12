4a13bfb28dc36d32a176ba1c74353bc2a6a9d8c8
Subhadeep Das <61840607+sd535682@users.noreply.github.com>
2025-05-12 11:49:59 +0530

    feature/create-quotes (#8)
    * create quote api
    
    * basic form input for creating quotes

---
+++ src/services/quoteAPI.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import {BASE_URL} from './authAPI';
+import {getToken} from '../utils/authStorage';
+
+export const quoteAPI = axios.create({
+  baseURL: BASE_URL,
+  headers: {'Content-Type': 'application/json'},
+});
+
+export interface CreateQuote {
+  quote: string;
+  author: string;
+  category: string;
+}
+
+export async function createQuote(quote: CreateQuote) {
+  try {
+    const token = await getToken();
+    if (!token) {
+      console.warn('No token found!');
+      return;
+    }
+
+    const response = await quoteAPI.post(
+      '/api/v1/quotes',
+      {
+        quote: quote.quote,
+        author: quote.author,
+        category: quote.category,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    console.log('quote created', response.data);
+    return response.data;
+  } catch (error) {
+    console.log('Quote API error:', error);
+  }
+}
