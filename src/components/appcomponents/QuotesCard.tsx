bdcbc7abd5682fc222ae92bc8d13fee524a28caf
Subhadeep Das <61840607+sd535682@users.noreply.github.com>
2025-05-14 20:17:11 +0530

    feature/public-quotes-route (#12)
    * change route and interface for public feed
    
    * added user info

--- src/components/appcomponents/QuotesCard.tsx
+++ src/components/appcomponents/QuotesCard.tsx
@@ -5,6 +5,9 @@ import {Quote} from '../../services/feedAPI';
 export default function QuotesCard({quote}: {quote: Quote}) {
   return (
     <View style={styles.container}>
+      <View style={styles.userContainer}>
+        <Text style={styles.userName}>{quote.user.name}</Text>
+      </View>
       <Text style={styles.categoryText}>{quote.category}</Text>
       <Text style={styles.quoteText}>{quote.quote}</Text>
       <Text style={styles.authorText}>{quote.author}</Text>
@@ -28,9 +31,20 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: Colors.black,
     textAlign: 'right',
+    fontStyle: 'italic',
   },
   categoryText: {
     fontSize: 12,
     color: Colors.textGray,
+  },
+  userContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  userName: {
+    fontSize: 12,
+    color: Colors.black,
+    fontWeight: 'bold',
   },
 });
