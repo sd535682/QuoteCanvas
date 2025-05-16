4424d6d7c7c1a2f9e7fbbed562a105fc8621095f
sd535682 <sd535682@gmail.com>
2025-05-16 13:27:56 +0530

    ui enhancement

--- src/screens/AppScreens/ProfileScreen.tsx
+++ src/screens/AppScreens/ProfileScreen.tsx
@@ -4,28 +4,28 @@ import {useContext} from 'react';
 import {AuthContext} from '../../context/AuthContext';
 import Lucide from '@react-native-vector-icons/lucide';
 
-export default function ProfileScreen({navigation}: {navigation: any}) {
+export default function ProfileScreen() {
   const {logout} = useContext(AuthContext);
   const {user} = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
-        <TouchableOpacity
-          style={styles.backButton}
-          onPress={() => navigation.navigate('Home')}>
-          <Lucide name="arrow-left" size={24} color={Colors.white} />
-        </TouchableOpacity>
+        <Text style={styles.title}>Profile</Text>
+        <Lucide name="quote" size={24} color={Colors.white} />
       </View>
-      <Text style={styles.text}>Hello, {user?.name}</Text>
-      <View style={styles.text}>
-        <Lucide name="mail" size={24} color={Colors.textGray} />
-        <Text style={styles.subtitle}>{user?.email}</Text>
-      </View>
-      <TouchableOpacity style={styles.button} onPress={logout}>
-        <Lucide name="log-out" size={28} color={Colors.textGray} />
-        <Text style={styles.subtitle}>Logout</Text>
-      </TouchableOpacity>
+      <>
+        <View style={styles.profileContainer}>
+          <Text style={styles.userName}>Hello, {user?.name}</Text>
+          <Text style={styles.subtitle}>{user?.email}</Text>
+        </View>
+        <View style={styles.profileContainer}>
+          <TouchableOpacity style={styles.button} onPress={logout}>
+            <Lucide name="log-out" size={28} color={Colors.black} />
+            <Text style={styles.subtitle}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      </>
     </View>
   );
 }
@@ -34,25 +34,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.background,
-    padding: 20,
+    paddingHorizontal: 20,
+    paddingBottom: '19%',
   },
   headerContainer: {
-    padding: 20,
-  },
-  backButton: {
-    borderColor: Colors.borderGray,
-    borderWidth: 1,
-    borderRadius: 20,
-    width: 40,
-    height: 40,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 20,
+    gap: 5,
   },
-  text: {
-    fontSize: 16,
+  title: {
+    fontSize: 24,
     fontWeight: 'bold',
     color: Colors.text,
-    marginTop: 20,
+    textAlign: 'center',
+  },
+  userName: {
+    fontSize: 20,
+    color: Colors.black,
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  profileContainer: {
+    backgroundColor: Colors.white,
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  button: {
     flexDirection: 'row',
     alignItems: 'center',
     gap: 5,
@@ -60,12 +70,6 @@ const styles = StyleSheet.create({
   subtitle: {
     fontSize: 14,
     fontWeight: '500',
-    color: Colors.textGray,
-  },
-  button: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 5,
-    marginTop: '30%',
+    color: Colors.black,
   },
 });
