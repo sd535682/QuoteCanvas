377c45c455c7666097e0b1344a78c20894145129
sd535682 <sd535682@gmail.com>
2025-05-16 22:51:05 +0530

    created separate screen for my quotes

--- src/screens/AppScreens/MyQuotesScreen.tsx
+++ src/screens/AppScreens/MyQuotesScreen.tsx
@@ -1,9 +1,108 @@
-import {View, Text} from 'react-native';
+import {View, StyleSheet, Text, ActivityIndicator} from 'react-native';
+import {Colors} from '../../constants/Colors';
+import {useEffect} from 'react';
+import {useState} from 'react';
+import QuotesCard from '../../components/appcomponents/QuotesCard';
+import Lucide from '@react-native-vector-icons/lucide';
+import {LegendList} from '@legendapp/list';
+import {getMyQuotes} from '../../services/myQuotesAPI';
+import {Quote} from '../../services/feedAPI';
 
 export default function MyQuotesScreen() {
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  const fetchQuotes = async () => {
+    try {
+      const data = await getMyQuotes();
+      setQuotes(data?.data || []);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    try {
+      const data = await getMyQuotes();
+      setQuotes(data?.data || []);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setRefreshing(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>My Quotes</Text>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.title}>Quotes</Text>
+        <Lucide name="quote" size={24} color={Colors.white} />
+      </View>
+      <LegendList
+        data={quotes}
+        renderItem={({item}) => <QuotesCard quote={item} />}
+        keyExtractor={item => item._id}
+        contentContainerStyle={styles.flatList}
+        recycleItems={true}
+        estimatedItemSize={200}
+        showsVerticalScrollIndicator={false}
+        onRefresh={onRefresh}
+        refreshing={refreshing}
+        ListEmptyComponent={
+          <View style={styles.emptyContainer}>
+            {loading ? (
+              <ActivityIndicator size="large" color={Colors.primary} />
+            ) : (
+              <Text style={styles.emptyText}>No quotes found 🥺</Text>
+            )}
+          </View>
+        }
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    paddingHorizontal: 20,
+    paddingBottom: '19%',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 20,
+    gap: 5,
+  },
+  flatList: {
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+    gap: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.text,
+    textAlign: 'center',
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '100%',
+  },
+  emptyText: {
+    fontSize: 16,
+    color: Colors.textGray,
+  },
+});
