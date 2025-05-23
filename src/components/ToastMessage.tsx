bb144a7e49a46a89afa010c7a58b4e4e3c2e38f2
sd535682 <sd535682@gmail.com>
2025-05-23 18:25:40 +0530

    customized toast

--- src/components/ToastMessage.tsx
+++ src/components/ToastMessage.tsx
@@ -1,5 +1,7 @@
 import {StyleSheet} from 'react-native';
 import Toast, {BaseToast, ErrorToast} from 'react-native-toast-message';
+import Lucide from '@react-native-vector-icons/lucide';
+import {CommonColors} from '../constants/Colors';
 
 export const showToast = (type: string, title: string, message: string) => {
   Toast.show({
@@ -16,37 +18,65 @@ export const toastConfig = {
   success: (props: any) => (
     <BaseToast
       {...props}
-      style={[styles.baseToast, {borderLeftColor: '#4CAF50'}]}
+      style={[styles.baseToast, styles.successBorderLeftColor]}
       contentContainerStyle={styles.contentContainer}
       text1Style={styles.text1}
       text2Style={styles.text2}
+      renderLeadingIcon={() => (
+        <Lucide
+          name="circle-check"
+          size={24}
+          color={CommonColors.notificationSuccessIcon}
+        />
+      )}
     />
   ),
   error: (props: any) => (
     <ErrorToast
       {...props}
-      style={[styles.baseToast, {borderLeftColor: '#f44336'}]}
+      style={[styles.baseToast, styles.errorBorderLeftColor]}
       contentContainerStyle={styles.contentContainer}
       text1Style={styles.text1}
       text2Style={styles.text2}
+      renderLeadingIcon={() => (
+        <Lucide
+          name="circle-x"
+          size={24}
+          color={CommonColors.notificationErrorIcon}
+        />
+      )}
     />
   ),
   info: (props: any) => (
     <BaseToast
       {...props}
-      style={[styles.baseToast, {borderLeftColor: '#2196F3'}]}
+      style={[styles.baseToast, styles.infoBorderLeftColor]}
       contentContainerStyle={styles.contentContainer}
       text1Style={styles.text1}
       text2Style={styles.text2}
+      renderLeadingIcon={() => (
+        <Lucide
+          name="info"
+          size={24}
+          color={CommonColors.notificationInfoIcon}
+        />
+      )}
     />
   ),
   warn: (props: any) => (
     <BaseToast
       {...props}
-      style={[styles.baseToast, {borderLeftColor: '#FF9800'}]}
+      style={[styles.baseToast, styles.warnBorderLeftColor]}
       contentContainerStyle={styles.contentContainer}
       text1Style={styles.text1}
       text2Style={styles.text2}
+      renderLeadingIcon={() => (
+        <Lucide
+          name="triangle-alert"
+          size={24}
+          color={CommonColors.notificationWarnIcon}
+        />
+      )}
     />
   ),
 };
@@ -54,8 +84,11 @@ export const toastConfig = {
 const styles = StyleSheet.create({
   baseToast: {
     borderLeftWidth: 8,
-    backgroundColor: 'rgba(7, 59, 76, 0.75)',
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
     borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 15,
   },
   contentContainer: {
     paddingHorizontal: 15,
@@ -63,10 +96,22 @@ const styles = StyleSheet.create({
   text1: {
     fontSize: 16,
     fontWeight: 'bold',
-    color: '#fff',
+    color: CommonColors.notificationText,
   },
   text2: {
     fontSize: 14,
-    color: '#fff',
+    color: CommonColors.notificationText,
+  },
+  successBorderLeftColor: {
+    borderLeftColor: CommonColors.notificationSuccessIcon,
+  },
+  errorBorderLeftColor: {
+    borderLeftColor: CommonColors.notificationErrorIcon,
+  },
+  infoBorderLeftColor: {
+    borderLeftColor: CommonColors.notificationInfoIcon,
+  },
+  warnBorderLeftColor: {
+    borderLeftColor: CommonColors.notificationWarnIcon,
   },
 });
